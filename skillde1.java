8b581460418290fd15b0f437a923c291f3d0bd30
TooMuchFish <shaodongshan@gmail.com>
2022-05-01 17:58:37 -0700

    new changes

---
+++ skillde1.java
@@ -0,0 +1,10 @@
+public class skillde1{
+    public static boolean divisible(double m, double n){
+        if(m%n == 0 ){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ skillde1Tester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class skillde1Tester {
+    @Test
+    public void divisible(){
+        assertEquals(true, skillde1.divisible(1, 2);
+        // leave errors to fix later
+    }
+}
